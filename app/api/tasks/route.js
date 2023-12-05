dd5b8b53b72a72fe82c2c7f6575ab8a8eea0a3b9
Rawand A.Ali <rawandbakr82@gmail.com>
2023-12-05 22:45:17 +0300

    modified:   app/api/tasks/route.js

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -26,14 +26,3 @@ export async function POST(req) {
     return NextResponse.json({ message: "Error", err }, { status: 500 });
   }
 }
-export async function DELETE(req) {
-  try {
-    const body = await req.json();
-    const {taskData} = body;
-    const res = await Task.findOneAndDelete(taskData.title)
-    return NextResponse.json({ message: "Task Created" }, { status: 201 });
-  } catch (error) {
-    console.log(error);
-    return new NextResponse("error in deletin task" + error, { status: 500 });
-  }
-}
