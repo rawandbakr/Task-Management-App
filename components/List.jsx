a5edf97de67e35fd7d5ea6a5e429029cea52b77a
Rawand A.Ali <rawandbakr82@gmail.com>
2023-12-05 22:44:44 +0300

    new file:   app/api/tasks/[id]/route.js 	modified:   app/api/tasks/route.js 	modified:   components/List.jsx

--- components/List.jsx
+++ components/List.jsx
@@ -1,6 +1,18 @@
 import { Button } from "./ui/button";
 import { TableCell, TableRow } from "@/components/ui/table";
+import { useRouter } from "next/navigation";
+
 const List = ({task}) => {
+  const router = useRouter();
+
+  const deleteTask = async () => {
+    const res = await fetch(`http://localhost:3000/api/tasks/${task._id}`, {
+      method: "DELETE",
+    });
+    if (res.ok) {
+      router.refresh();
+    }
+  };
   return (
     <TableRow key={task._id}>
       <TableCell className="font-medium">{task.title}</TableCell>
@@ -9,7 +21,7 @@ const List = ({task}) => {
       <TableCell className="text-right">
         <span className="flex flex-row justify-end gap-2">
           <Button variant="outline">Edit</Button>
-          <Button variant="destructive">Delete</Button>
+          <Button variant="destructive" onClick={deleteTask}>Delete</Button>
         </span>
       </TableCell>
     </TableRow>
