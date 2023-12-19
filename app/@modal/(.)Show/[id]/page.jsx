f7d1f3a05bebd461fc9a046193b13cccccc6c585
Rawand A.Ali <rawandbakr82@gmail.com>
2023-12-19 20:50:13 +0300

    modified:   app/@modal/(.)Show/[id]/page.jsx 	modified:   components/Modal.jsx

--- app/@modal/(.)Show/[id]/page.jsx
+++ app/@modal/(.)Show/[id]/page.jsx
@@ -6,9 +6,12 @@ export default async function Show({ params }) {
   const task = await GetTasksId(id);
   return (
     <Modal>
-      <h1 className=" m-2 p-4 rounded-md w-full">{task.title}</h1>
-      <h1 className=" m-2 p-4 rounded-md w-full">{task.description}</h1>
-      <Status status={task.status} />
+      <div className="bg-white p-6 rounded-xl w-fit h-fit ">
+        
+        <h1 className=" m-2 p-4 rounded-md w-full">{task.title}</h1>
+        <h1 className=" m-2 p-4 rounded-md w-full">{task.description}</h1>
+        <Status status={task.status} />
+      </div>
     </Modal>
   );
 }
