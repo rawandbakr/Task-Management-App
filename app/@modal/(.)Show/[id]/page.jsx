281b9736e1dc740be980661431e81d67cc19c074
Rawand A.Ali <rawandbakr82@gmail.com>
2023-12-19 20:30:53 +0300

    modified:   app/@modal/(.)Show/[id]/page.jsx 	renamed:    components/Modal/Modal.jsx -> components/Modal.jsx

--- app/@modal/(.)Show/[id]/page.jsx
+++ app/@modal/(.)Show/[id]/page.jsx
@@ -1,16 +1,14 @@
 import Status from "@/components/Form/Status";
-import Modal from "@/components/modal/Modal";
+import Modal from "@/components/Modal";
 import { GetTasksId } from "@/lib/data";
 export default async function Show({ params }) {
   const id = params.id;
   const task = await GetTasksId(id);
   return (
     <Modal>
-        <h1 className=" m-2 p-4 rounded-md w-full">{task.title}</h1>
-        <h1 className=" m-2 p-4 rounded-md w-full">
-          {task.description}
-        </h1>
-        <Status status={task.status} />
+      <h1 className=" m-2 p-4 rounded-md w-full">{task.title}</h1>
+      <h1 className=" m-2 p-4 rounded-md w-full">{task.description}</h1>
+      <Status status={task.status} />
     </Modal>
   );
 }
