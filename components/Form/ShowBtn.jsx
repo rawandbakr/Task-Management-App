5fa1aad1b86125a9b1735fe7a9b851430e9b190e
Rawand A.Ali <rawandbakr82@gmail.com>
2023-12-19 20:27:07 +0300

    modified:   app/@modal/(.)Show/[id]/page.jsx 	modified:   components/Form/ShowBtn.jsx 	modified:   components/Modal/Modal.jsx

--- components/Form/ShowBtn.jsx
+++ components/Form/ShowBtn.jsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 
 const ShowBtn = ({ id }) => {
   return (
-    <Button variant="default">
-      <Link href={`/Show/${id}`} className=" items-center justify-center">...</Link>
+    <Button variant="outline">
+      <Link href={`/Show/${id}`} className=" items-center justify-center">
+        ...
+      </Link>
     </Button>
   );
 };
