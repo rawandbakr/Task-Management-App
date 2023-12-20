e60b46794e624b1e8ed3d693c4ad5afd4eba934c
Rawand A.Ali <rawandbakr82@gmail.com>
2023-12-20 22:57:54 +0300

    modified:   app/api/auth/[...nextauth]/options.js

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,9 +1,21 @@
 import GitHubProvider from "next-auth/providers/github";
+import PostgresAdapter from "@auth/pg-adapter";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host:process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
 export const options = {
+  adapter: PostgresAdapter(pool),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_Secret,
-    })
+    }),
   ],
 };
